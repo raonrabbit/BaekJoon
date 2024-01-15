9a590d0eb13b4dc413fe4c7866aad288a88af213
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-01-16 00:56:15 +0900

    [Silver V] Title: 오늘도 졌다, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Silver/14582. 오늘도 졌다/오늘도 졌다.cc
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+int main() {
+	int jeminis[9];
+	int gulibus[9];
+	int score = 0;
+	bool yukjunpae = false;
+	for (int i = 0; i < 9; i++) {
+		scanf("%d", &jeminis[i]);
+	}
+	for (int i = 0; i < 9; i++) {
+		scanf("%d", &gulibus[i]);
+	}
+	for (int i = 0; i < 9; i++) {
+		score += jeminis[i];
+		if (score > 0) yukjunpae = true;
+		else if (score == 0) yukjunpae = false;
+		score -= gulibus[i];
+		if (score > 0) yukjunpae = true;
+		else if (score == 0) yukjunpae = false;
+	}
+
+	if (yukjunpae) printf("Yes");
+	else printf("No");
+}
