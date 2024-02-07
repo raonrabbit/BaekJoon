168390a27b315679b4f0069288d3bf4ccfa4ecee
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-02-07 23:57:16 +0900

    [Silver V] Title: 포켓몬 GO, Time: 64 ms, Memory: 5588 KB -BaekjoonHub

--- 백준/Silver/13717. 포켓몬 GO/포켓몬 GO.cs
+++ 백준/Silver/13717. 포켓몬 GO/포켓몬 GO.cs
@@ -1,23 +1,29 @@
-string resultPokemon = "";
-int evolutionCount = 0;
-int max = 0;
-int pokeCount = Int32.Parse(Console.ReadLine());
-for(int i = 0; i < pokeCount; i++)
+class Program
 {
-    string pokemon = Console.ReadLine();
-    int[] candyInfo = Array.ConvertAll(Console.ReadLine().Split(), Int32.Parse);
-    int currentEvolutionCount = 0;
-    while (candyInfo[0] <= candyInfo[1])
+    static void Main(string[] args)
     {
-        currentEvolutionCount++;
-        candyInfo[1] = candyInfo[1] - candyInfo[0] + 2;
-    }
-    evolutionCount += currentEvolutionCount;
-    if(max < currentEvolutionCount)
-    {
-        resultPokemon = pokemon;
-        max = currentEvolutionCount;
+        string resultPokemon = "";
+        int evolutionCount = 0;
+        int max = 0;
+        int pokeCount = Int32.Parse(Console.ReadLine());
+        for (int i = 0; i < pokeCount; i++)
+        {
+            string pokemon = Console.ReadLine();
+            int[] candyInfo = Array.ConvertAll(Console.ReadLine().Split(), Int32.Parse);
+            int currentEvolutionCount = 0;
+            while (candyInfo[0] <= candyInfo[1])
+            {
+                currentEvolutionCount++;
+                candyInfo[1] = candyInfo[1] - candyInfo[0] + 2;
+            }
+            evolutionCount += currentEvolutionCount;
+            if (max < currentEvolutionCount)
+            {
+                resultPokemon = pokemon;
+                max = currentEvolutionCount;
+            }
+        }
+        Console.WriteLine(evolutionCount);
+        Console.WriteLine(resultPokemon);
     }
 }
-Console.WriteLine(evolutionCount);
-Console.WriteLine(resultPokemon);
