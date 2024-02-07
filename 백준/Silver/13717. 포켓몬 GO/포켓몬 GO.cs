420d2f36516cb63779e7551db14af0aebaa3fad4
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-02-07 23:55:25 +0900

    [Silver V] Title: 포켓몬 GO, Time: 68 ms, Memory: 5596 KB -BaekjoonHub

--- 백준/Silver/13717. 포켓몬 GO/포켓몬 GO.cs
+++ 백준/Silver/13717. 포켓몬 GO/포켓몬 GO.cs
@@ -5,7 +5,7 @@ int pokeCount = Int32.Parse(Console.ReadLine());
 for(int i = 0; i < pokeCount; i++)
 {
     string pokemon = Console.ReadLine();
-    List<int> candyInfo = Array.ConvertAll(Console.ReadLine().Split(), Int32.Parse).ToList<int>();
+    int[] candyInfo = Array.ConvertAll(Console.ReadLine().Split(), Int32.Parse);
     int currentEvolutionCount = 0;
     while (candyInfo[0] <= candyInfo[1])
     {
