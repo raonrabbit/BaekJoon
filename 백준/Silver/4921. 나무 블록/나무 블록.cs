0b9e484d316c86e09d2ca3001682c161bd7e35e3
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-02-11 00:33:44 +0900

    [Silver IV] Title: 나무 블록, Time: 112 ms, Memory: 9660 KB -BaekjoonHub

--- 백준/Silver/4921. 나무 블록/나무 블록.cs
+++ 백준/Silver/4921. 나무 블록/나무 블록.cs
@@ -1,12 +1,12 @@
-var blockInfo = new List<int[]>() { 
-    new int[] { 0, -1},
-    new int[] { -1, 0 },
-    new int[] { -1, -1 },
-    new int[] { 1, 1 },
-    new int[] { 1, -2 },
-    new int[] { -2, 1 },
-    new int[] { -2, -2 },
-    new int[] { 2, 2 } 
+var info = new int[8, 2] { 
+    { 0, -1},
+    { -1, 0 },
+    { -1, -1 },
+    { 1, 1 },
+    { 1, -2 },
+    { -2, 1 },
+    { -2, -2 },
+    { 2, 2 } 
 };
 var result = new List<bool>();
 while (true)
@@ -22,14 +22,15 @@ while (true)
     int right = -1;
     foreach (var n in input.Substring(1))
     {
-        if (right == 0 || right + blockInfo[n - '0' - 1][0] != 0) isValid = false;
-        right = blockInfo[n - '0' - 1][1];
+        var t = n - 1 - '0';
+        if (right == 0 || right + info[t, 0] != 0) isValid = false;
+        right = info[t, 1];
     }
     result.Add(isValid);
 }
 
 for(int i = 1; i <= result.Count; i++)
 {
-    if (result[i - 1]) Console.WriteLine(i + ". VALID");
-    else Console.WriteLine(i + ". NOT");
+    if (result[i - 1]) Console.WriteLine($"{i}. VALID");
+    else Console.WriteLine($"{i}. NOT");
 }
