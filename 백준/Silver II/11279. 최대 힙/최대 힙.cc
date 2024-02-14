ef77c35e8c90f93a7c0786716450c929e525ee10
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-02-15 03:05:06 +0900

    [Silver II] Title: 최대 힙, Time: 48 ms, Memory: 3744 KB -BaekjoonHub

---
+++ 백준/Silver II/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<map>
+#include<vector>
+using namespace std;
+int main() {
+	int n;
+	unsigned int a;
+	scanf("%d", &n);
+	vector<unsigned int> r;
+	map<unsigned int, int, greater<int>> s;
+	int b;
+	for (int i = 0; i < n; i++) {
+		scanf("%d", &a);
+		if (a == 0) {
+			if (s.size() == 0) r.push_back(0);
+			else {
+				b = s.begin()->first;
+				r.push_back(b);
+				s.begin()->second--;
+				if(s.begin()->second == 0) s.erase(b);
+			}
+		}
+		else {
+			if (s.find(a) != s.end()) s[a]++;
+			else s.insert({ a, 1 });
+		}
+	}
+	for (unsigned int i : r) printf("%d\n", i);
+}
