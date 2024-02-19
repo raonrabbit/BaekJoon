50197c1c624c1fc314f470d763313426305a6d4d
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-02-19 22:52:40 +0900

    [Silver IV] Title: 우당탕탕 영화예매, Time: 144 ms, Memory: 18328 KB -BaekjoonHub

--- 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.cs
+++ 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.cs
@@ -1,20 +1,19 @@
 var input = Array.ConvertAll(Console.ReadLine().Split(), Int32.Parse);
 int result = 0;
+int count = 0;
 for (int i = 0; i < input[0]; i++)
 {
     var seat = Console.ReadLine();
-    for(int idx = 0; idx <= input[1] - input[2]; idx++)
+    count = 0;
+    for(int idx = 0; idx < input[1]; idx++)
     {
-        bool b = true;
-        for(int j = 0; j < input[2]; j++)
-        {
-            if (seat[idx + j] != '0')
-            {
-                b = false;
-                break;
-            }
+        if (seat[idx] == '0') count++;
+        else {
+            if (count >= input[2]) 
+                result += count - input[2] + 1;
+            count = 0;
         }
-        if (b) result++;
     }
+    if (count >= input[2]) result += count - input[2] + 1;
 }
 Console.WriteLine(result);
