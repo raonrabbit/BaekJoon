e9a454a05ed6755d2f97beee2c2a3b4175eaa5cc
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-02-21 01:28:50 +0900

    [Bronze I] Title: 카드 바꿔치기, Time: 76 ms, Memory: 6244 KB -BaekjoonHub

--- 백준/Bronze/18766. 카드 바꿔치기/카드 바꿔치기.cs
+++ 백준/Bronze/18766. 카드 바꿔치기/카드 바꿔치기.cs
@@ -1,7 +1,6 @@
-int testcase = Int32.Parse(Console.ReadLine());
-while(testcase-- != 0)
+int testcase=Int32.Parse(Console.ReadLine());
+while(testcase--!=0)
 {
     Console.ReadLine();
-    if (Enumerable.SequenceEqual(Console.ReadLine().Split().OrderBy(a => a), Console.ReadLine().Split().OrderBy(a => a))) Console.WriteLine("NOT CHEATER");
-    else Console.WriteLine("CHEATER");
+    Console.WriteLine(Enumerable.SequenceEqual(Console.ReadLine().Split().OrderBy(a=>a),Console.ReadLine().Split().OrderBy(a=>a))?"NOT CHEATER":"CHEATER");
 }
