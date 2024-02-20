d547f0fd6ba7942a22de8aee240fa7ca71860010
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-02-21 00:52:36 +0900

    [Silver IV] Title: 볼링공 찾아주기, Time: 540 ms, Memory: 33316 KB -BaekjoonHub

--- 백준/Silver/28446. 볼링공 찾아주기/볼링공 찾아주기.cs
+++ 백준/Silver/28446. 볼링공 찾아주기/볼링공 찾아주기.cs
@@ -1,10 +1,10 @@
 using StreamWriter sw = new(Console.OpenStandardOutput());
 
-int n = Int32.Parse(Console.ReadLine()!);
+int n = Int32.Parse(Console.ReadLine());
 var d = new Dictionary<int, int>();
 for (int i = 0; i < n; i++)
 {
-    var l = Array.ConvertAll(Console.ReadLine()!.Split(), Int32.Parse);
+    var l = Array.ConvertAll(Console.ReadLine().Split(), Int32.Parse);
     if (l[0] == 1) d.Add(l[2], l[1]);
     else sw.WriteLine(d[l[1]]);
 }
