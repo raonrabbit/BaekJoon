3f02e3980e4bccec6b219da80d4df72aa6a06729
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-02-20 22:24:43 +0900

    [Bronze I] Title: 선로에 마네킹이야!!, Time: 64 ms, Memory: 5452 KB -BaekjoonHub

--- 백준/Bronze/15920. 선로에 마네킹이야！！/선로에 마네킹이야！！.cs
+++ 백준/Bronze/15920. 선로에 마네킹이야！！/선로에 마네킹이야！！.cs
@@ -1,3 +1,3 @@
 Console.ReadLine();
 var l=Console.ReadLine().Split('W');
-Console.WriteLine((l.Length<3)?0:(l[1].Length!=0)?6:l[0].Length%2==0?5:1);
+Console.WriteLine(l.Length<3?0:l[1].Length!=0?6:l[0].Length%2==0?5:1);
