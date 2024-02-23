19a2c51c15670c351d0119632da2223f35ea0ca7
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-02-24 01:32:32 +0900

    [Silver IV] Title: 야근, Time: 152 ms, Memory: 18868 KB -BaekjoonHub

---
+++ 백준/Silver/14402. 야근/야근.cs
@@ -0,0 +1,14 @@
+int count = Int32.Parse(Console.ReadLine()), result = 0;
+var m = new Dictionary<string, Stack<string>>();
+while(count-- != 0){
+    var l = Console.ReadLine().Split();
+    if (!m.ContainsKey(l[0]))
+    {
+        m.Add(l[0], new Stack<string>(new string[] { l[1] }));
+        continue;
+    }
+    if (m[l[0]].Count != 0 && m[l[0]].Peek() == "+" && l[1] == "-") m[l[0]].Pop();
+    else m[l[0]].Push(l[1]);
+}
+foreach(var a in m) { result += a.Value.Count; }
+Console.WriteLine(result);
