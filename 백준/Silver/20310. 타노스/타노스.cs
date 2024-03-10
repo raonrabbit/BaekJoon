882172d43a96406b02ef92b26ec106719f56b3d1
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-03-11 00:33:15 +0900

    [Silver III] Title: 타노스, Time: 76 ms, Memory: 5876 KB, Score: 25 point -BaekjoonHub

---
+++ 백준/Silver/20310. 타노스/타노스.cs
@@ -0,0 +1,26 @@
+var s = Console.ReadLine().ToArray().Select(x => x.ToString()).ToList();
+int zC = 0, oC = 0;
+foreach(var c in s)
+{
+    if (c == "0") zC++;
+    else oC++;
+}
+zC /= 2;
+oC /= 2;
+for (int i = 0; i < s.Count; i++)
+{
+    if (oC != 0 && s[i] == "1")
+    {
+        s.RemoveAt(i);
+        oC--;
+    }
+}
+for(int i = s.Count - 1; i >= 0; i--) { 
+    if (zC != 0 && s[i] == "0")
+    {
+        s.RemoveAt(i);
+        zC--;
+    }
+}
+
+foreach (var c in s) Console.Write(c);
