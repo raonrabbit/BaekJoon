7c1bbc05f43be3a49196cf02aaaf57d1f2ffde6a
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-03-18 00:19:11 +0900

    [Silver IV] Title: 설탕 배달, Time: 64 ms, Memory: 5512 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.cs
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.cs
@@ -1,12 +1,4 @@
-var d = Int32.Parse(Console.ReadLine());
-int t = d / 3, p = d % 3, result = Int32.MaxValue;
-bool yeah = false;
-for(int i = 0; i <= t; i++){
-    var n = (d - (3 * i)) / 5;
-    if((d - (3 * i)) % 5 == 0)
-    {
-        result = Math.Min(result, i + n);
-        yeah = true;
-    }
-}
-Console.WriteLine(yeah?result:-1);
+var d=Int32.Parse(Console.ReadLine());
+int t=d/3,p=d%3,r=2000;
+for(int i=0;i<=t;i++)if((d-(3*i))%5==0)r=Math.Min(r,i+(d-(3*i))/5);
+Console.WriteLine(r==2000?-1:r);
