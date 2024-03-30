0292d9c960fb8cb0aef30698d2acdda3785a4168
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-03-31 03:09:19 +0900

    [Silver III] Title: 1로 만들기, Time: 0 ms, Memory: 1232 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.cc
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.cc
@@ -1,25 +1,47 @@
 #include<stdio.h>
-#include<limits.h>
-#include <algorithm>
+#include<set>
+#include<queue>
+
 using namespace std;
 
-int minCount = INT_MAX;
-void dp(int n, int count) {
-	if (n == 1) {
-		minCount = min(count, minCount);
-		return;
-	}
-	if (n < 0 || count > minCount) return;
-	count++;
-	if (n % 3 == 0) dp(n / 3, count);
-	if (n % 2 == 0) dp(n / 2, count);
-	dp(n - 1, count);
-	return;
-}
-
 int main() {
-	int n;
+	int n, t, result = -1;
+	queue<int> q;
+	set<int> set;
+
 	scanf("%d", &n);
-	dp(n, 0);
-	printf("%d", minCount);
+	q.push(n);
+	set.insert(n);
+
+	for (int i = 0; i < n; i++) {
+		int qSize = q.size();
+		for (int j = 0; j < qSize; j++) {
+			int d = q.front();
+
+			if (d == 1) {
+				result = i;
+				break;
+			}
+			if (d % 2 == 0 && set.find(d / 2) == set.end()) {
+				set.insert(d / 2);
+				q.push(d / 2);
+			}
+
+			if (d % 3 == 0 && set.find(d / 3) == set.end()) {
+				set.insert(d / 3);
+				q.push(d / 3);
+			}
+
+			if (set.find(d - 1) == set.end()) {
+				set.insert(d - 1);
+				q.push(d - 1);
+			}
+
+			q.pop();
+		}
+
+		if (result != -1) break;
+	}
+
+	printf("%d", result);
 }
