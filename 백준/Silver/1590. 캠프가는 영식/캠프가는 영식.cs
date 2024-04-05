b73f0467ac748a7d6ee302436e4b55dcea659a94
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-04-06 01:23:03 +0900

    [Silver IV] Title: 캠프가는 영식, Time: 72 ms, Memory: 5948 KB -BaekjoonHub

---
+++ 백준/Silver/1590. 캠프가는 영식/캠프가는 영식.cs
@@ -0,0 +1,10 @@
+var d = Console.ReadLine().Split().Select(int.Parse).ToList();
+int min = int.MaxValue;
+for(int i = 0; i < d[0]; i++)
+{
+    var t = Console.ReadLine().Split().Select(int.Parse).ToList();
+    if (t[0] + t[1] * (t[2] - 1) < d[1]) continue;
+    if (t[0] >= d[1]) min = Math.Min(min, t[0]);
+    else min = Math.Min(min, t[0] + t[1] * ((d[1] - t[0]) / t[1] + (((d[1] - t[0]) % t[1] == 0) ? 0 : 1)));
+}
+Console.Write(min == int.MaxValue ? -1 : min - d[1]);
