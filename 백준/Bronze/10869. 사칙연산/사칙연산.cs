426ae28f1e81faa17b85aca271156895db879e11
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-04-29 22:14:38 +0900

    [Bronze V] Title: 사칙연산, Time: 68 ms, Memory: 5744 KB -BaekjoonHub

---
+++ 백준/Bronze/10869. 사칙연산/사칙연산.cs
@@ -0,0 +1,6 @@
+var l = Console.ReadLine().Split().Select(int.Parse).ToList();
+Console.WriteLine(l[0] + l[1]);
+Console.WriteLine(l[0] - l[1]);
+Console.WriteLine(l[0] * l[1]);
+Console.WriteLine(l[0] / l[1]);
+Console.WriteLine(l[0] % l[1]);
