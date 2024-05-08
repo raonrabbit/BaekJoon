b38485fc0e8e9692d00730bb40611d03247de56e
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-05-09 01:17:31 +0900

    [Bronze V] Title: A+B - 8, Time: 76 ms, Memory: 6404 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cs
@@ -0,0 +1,7 @@
+var n = int.Parse(Console.ReadLine());
+
+for(int i = 0; i < n; i++){
+    var l = Console.ReadLine().Split().Select(int.Parse).ToList();
+    
+    Console.WriteLine($"Case #{i+1}: {l[0]} + {l[1]} = {l[0] + l[1]}");
+}
