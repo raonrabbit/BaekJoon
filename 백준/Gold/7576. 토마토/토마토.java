4aa71628c50220368f8aa4edc45b2bc3fdf35f4f
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-07-14 20:49:21 +0900

    [Gold V] Title: 토마토, Time: 560 ms, Memory: 117508 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -13,12 +13,14 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-        Queue<Integer> q = new LinkedList<>();
+        Queue<int[]> q = new LinkedList<>();
         StringTokenizer st = new StringTokenizer(bf.readLine());
+
         M = Integer.parseInt(st.nextToken());
         N = Integer.parseInt(st.nextToken());
+
         boolean[][] visitedMap = new boolean[N][M];
-        int[][] chest = new int[N][M];
+
         int tomatoCount = 0;
         int goodTomatoCount = 0;
         int date = 0;
@@ -29,34 +31,37 @@ public class Main {
 
             for(int x = 0; x < M; x++){
                 int a = Integer.parseInt(st.nextToken());
-                chest[y][x] = a;
+
                 if(a == 1) {
-                    q.add(y * M + x);
+                    q.add(new int[]{x, y});
                     visitedMap[y][x] = true;
                 }
+                if(a == -1) visitedMap[y][x] = true;
                 if(a != -1) tomatoCount++;
             }
         }
+
         int[] dirX = {1, -1, 0, 0};
         int[] dirY = {0, 0, 1, -1};
+
         while(!q.isEmpty()){
             if(goodTomatoCount == tomatoCount) break;
             int qSize = q.size();
             date++;
             for(int i = 0; i < qSize; i++){
-                int t = q.remove();
-                int x = t % M;
-                int y = t / M;
+                int[] t= q.remove();
+                int x = t[0];
+                int y = t[1];
 
-                chest[y][x] = 1;
                 goodTomatoCount++;
 
                 int newX, newY;
                 for(int d = 0; d < 4; d++){
                     newX = x + dirX[d];
                     newY = y + dirY[d];
-                    if(!IsOutOfChest(newX, newY) && !visitedMap[newY][newX] && chest[newY][newX] == 0) {
-                        q.add((newY) * M + (newX));
+
+                    if(!IsOutOfChest(newX, newY) && !visitedMap[newY][newX]) {
+                        q.add(new int[]{newX, newY});
                         visitedMap[newY][newX] = true;
                     }
                 }
