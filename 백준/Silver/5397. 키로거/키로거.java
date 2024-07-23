56f538f00fd0446e81022febd5c3d88742e8dd18
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-07-24 08:51:08 +0900

    [Silver II] Title: 키로거, Time: 564 ms, Memory: 133564 KB -BaekjoonHub

---
+++ 백준/Silver/5397. 키로거/키로거.java
@@ -0,0 +1,91 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	public static class Node{
+		public Node left;
+		public Node right;
+		public char c;
+		
+		Node(char c){
+			this.c = c;
+		}
+	}
+	
+	public static class LinkedList{
+		Node head, tail;
+		
+		LinkedList(){
+			head = new Node('/');
+			tail = head;
+		}
+		
+		public void Add(Node cur, Node newNode) {
+			if(cur == head) head = newNode;
+			else cur.left.right = newNode;
+			
+			newNode.left = cur.left;
+			newNode.right = cur;
+			
+			cur.left = newNode;
+		}
+		
+		public void Remove(Node node) {
+			if(node == head) return;
+			
+			if(node.left == head) head = node;
+			else node.left.left.right = node;
+			node.left = node.left.left;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		//BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		int T = Integer.parseInt(br.readLine());
+		
+		for(int t = 0; t < T; t++) {
+			LinkedList text = new LinkedList();
+			
+			Node cursor = text.head;
+			
+			char[] instructionSet = br.readLine().toCharArray();
+			
+			for(char instruction : instructionSet) {
+				switch(instruction){
+				case '<':
+					if(cursor.left != null) cursor = cursor.left;
+					break;
+				
+				case '>':
+					if(cursor.right != null) cursor = cursor.right;
+					break;
+				
+				case '-':
+					text.Remove(cursor);
+					break;
+				
+				default:
+					text.Add(cursor, new Node(instruction));
+					break;
+				}
+			}
+			
+			Node temp = text.head;
+			StringBuilder sb = new StringBuilder();
+			while(temp != text.tail) {
+				//bw.write(temp.c);
+				sb.append(temp.c);
+				temp = temp.right;
+			}
+			System.out.println(sb);
+			//bw.flush();
+		}
+		br.close();
+		//bw.close();
+	}
+}
