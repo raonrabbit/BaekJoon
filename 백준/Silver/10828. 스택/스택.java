0b0c8f7ddad77134e0d3b33b4ceecf4206bd6e99
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-07-27 12:14:48 +0900

    [Silver IV] Title: 스택, Time: 168 ms, Memory: 19312 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -4,38 +4,41 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		int[] stack = new int[10001];
-		stack[0] = -1;
-		int curIndex = 0;
-		
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
-		int N = Integer.parseInt(br.readLine());
-		
-		StringTokenizer st;
-		for(int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			
-			switch(st.nextToken()) {
-			case "push":
-				stack[++curIndex] = Integer.parseInt(st.nextToken());
-				break;
-			case "pop":
-				System.out.println(stack[curIndex]);
-				if(curIndex > 0) curIndex--;
-				break;
-			case "top":
-				System.out.println(stack[curIndex]);
-				break;
-			case "size":
-				System.out.println(curIndex);
-				break;
-			case "empty":
-				if(curIndex <= 0) System.out.println(1);
-				else System.out.println(0);
-				break;
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        int[] stack = new int[10001];
+        stack[0] = -1;
+        int curIndex = 0;
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+        for(int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+
+            switch(st.nextToken()) {
+                case "push":
+                    stack[++curIndex] = Integer.parseInt(st.nextToken());
+                    break;
+                case "pop":
+                    sb.append(stack[curIndex]).append('\n');
+                    if(curIndex > 0) curIndex--;
+                    break;
+                case "top":
+                    sb.append(stack[curIndex]).append('\n');
+                    break;
+                case "size":
+                    sb.append(curIndex).append('\n');
+                    break;
+                case "empty":
+                    if(curIndex <= 0) sb.append(1).append('\n');
+                    else sb.append(0).append('\n');
+                    break;
+            }
+        }
+
+        System.out.print(sb);
+    }
 }
