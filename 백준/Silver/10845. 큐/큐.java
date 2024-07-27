6b44142d8c64d6596648c5718cc361f7664441c5
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-07-27 12:08:04 +0900

    [Silver IV] Title: 큐, Time: 188 ms, Memory: 19384 KB -BaekjoonHub

--- 백준/Silver/10845. 큐/큐.java
+++ 백준/Silver/10845. 큐/큐.java
@@ -5,44 +5,51 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-	public static void main(String[] args) throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-	
-		int N = Integer.parseInt(br.readLine());
-		int[] queue = new int[10000];
-		
-		int front = 0, back = 0;
-		
-		for(int i = 0; i < N; i++) {
-			StringTokenizer st = new StringTokenizer(br.readLine());
-			String inst = st.nextToken();
-			
-			switch(inst) {
-			case "push":
-				int n = Integer.parseInt(st.nextToken());
-				queue[back++] = n;
-				break;
-			case "pop":
-				if(back - front == 0) System.out.println(-1);
-				else System.out.println(queue[front++]);
-				break;
-			case "size":
-				System.out.println(back - front);
-				break;
-			case "empty":
-				if(back - front == 0) System.out.println(1);
-				else System.out.println(0);
-				break;
-			case "front":
-				if(back - front == 0) System.out.println(-1);
-				else System.out.println(queue[front]);
-				break;
-			case "back":
-				if(back - front == 0) System.out.println(-1);
-				else System.out.println(queue[back - 1]);
-				break;
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] queue = new int[10000];
+
+        int front = 0, back = 0;
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String inst = st.nextToken();
+
+            switch(inst) {
+                case "push":
+                    int n = Integer.parseInt(st.nextToken());
+                    queue[back++] = n;
+                    break;
+                case "pop":
+                    if(back - front == 0) sb.append(-1);
+                    else sb.append(queue[front++]);
+                    sb.append('\n');
+                    break;
+                case "size":
+                    sb.append(back - front);
+                    sb.append('\n');
+                    break;
+                case "empty":
+                    if(back - front == 0) sb.append(1);
+                    else sb.append(0);
+                    sb.append('\n');
+                    break;
+                case "front":
+                    if(back - front == 0) sb.append(-1);
+                    else sb.append(queue[front]);
+                    sb.append('\n');
+                    break;
+                case "back":
+                    if(back - front == 0) sb.append(-1);
+                    else sb.append(queue[back - 1]);
+                    sb.append('\n');
+                    break;
+            }
+        }
+        System.out.println(sb);
+    }
 
 }
