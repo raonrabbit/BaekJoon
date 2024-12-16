b215bd06e37afaa39c964b600379af116b60b752
raonrabbit <110987711+raonrabbit@users.noreply.github.com>
2024-12-16 23:29:58 +0900

    [Silver I] Title: 숨바꼭질, Time: 148 ms, Memory: 18992 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -10,6 +10,11 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
+        if(N == K){
+            System.out.print(0);
+            return;
+        }
+
         Queue<Integer> queue = new ArrayDeque<>();
         boolean[] visited = new boolean[200000];
 
@@ -19,24 +24,32 @@ public class Main {
             int n = queue.size();
             for(int i = 0; i < n; i++){
                 int cur = queue.poll();
-                if(cur == K){
-                    System.out.print(count);
-                    return;
-                }
 
                 if(cur + 1 < 200000 && !visited[cur + 1]){
                     queue.add(cur + 1);
                     visited[cur + 1] = true;
+                    if(cur + 1 == K){
+                        System.out.print(count + 1);
+                        return;
+                    }
                 }
 
                 if(cur - 1 >= 0 && !visited[cur - 1]){
                     queue.add(cur - 1);
                     visited[cur - 1] = true;
+                    if(cur - 1 == K){
+                        System.out.print(count + 1);
+                        return;
+                    }
                 }
 
                 if(cur * 2 < 200000 && !visited[cur * 2]){
                     queue.add(cur * 2);
                     visited[cur * 2] = true;
+                    if(cur * 2 == K){
+                        System.out.print(count + 1);
+                        return;
+                    }
                 }
             }
             count++;
